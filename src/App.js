18175bf63d32b40e9e9d69268c56a7d29e2964f5
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-04-19 06:16:50 +0500

    lesson-51

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import ComponentA from './components/ComponentA';
 
 import './App.css';
-
+import { useState } from 'react';
 function App() {
-
+  const [color, setColor] = useState(false);
+  const handleChanger = () => {
+    setColor(!color)
+  }
   console.log('Hello World');
   return (
     <>
-      <ComponentA />
-      <h2>App js</h2>
+      <div className={color ? 'active' : 'passive'}>
+        <button className='btn' onClick={handleChanger}>
+          {color ? 'Light' : 'Dark'}
+        </button>
+      </div>
+
     </>
   );
 }
